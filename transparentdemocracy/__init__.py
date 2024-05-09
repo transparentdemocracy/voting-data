810ca5732a16b3b0ddfd70d6b96b519ce097087f
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-05-09 10:10:37 +0200

    Replaced usages of ACTOR_JSON_INPUT_PATH with CONFIG.plenary_html_input_path(...)
    Accessing data through CONFIG.* will give us more flexibility with the data directory

--- transparentdemocracy/__init__.py
+++ transparentdemocracy/__init__.py
@@ -4,9 +4,6 @@ from config import CONFIG
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "data")
 
-# input
-ACTOR_JSON_INPUT_PATH = os.path.join(DATA_PATH, "input", "actors", "actor")
-
 # output
 PLENARY_MARKDOWN_OUTPUT_PATH = os.path.join(DATA_PATH, "output", "plenary", "markdown")
 PLENARY_JSON_OUTPUT_PATH = os.path.join(DATA_PATH, "output", "plenary", "json")
