bc3aed92e0cfc5dac415977e6a146649e6b1f237
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-05-21 17:08:00 +0200

    Send output of run-all.sh to different files, this makes it easier to figure out what failed.
    Idea: rewrite using bats (https://github.com/bats-core)

--- run-all.sh
+++ run-all.sh
@@ -3,14 +3,15 @@
 set -e
 set -x
 
-./test.sh
+#./test.sh
 
-td-plenaries-markdown 2>&1 >out/td-plenaries-markdown.out 
-td-plenaries-json 2>&1 >td-plenaries-json
-td-votes-json 2>&1 >td-votes-json
-td-politicians-json 2>&1 >td-politicians-json
-td-print-politicians-by-fraction 2>&1 >td-print-politicians-by-fraction
-td-download-referenced-documents 2>&1 >td-download-referenced-documents
+mkdir -p out
+td-plenaries-markdown >out/td-plenaries-markdown.out 2>&1
+td-plenaries-json >td-plenaries-json 2>&1
+td-votes-json >td-votes-json 2>&1
+td-politicians-json >td-politicians-json 2>&1
+td-print-politicians-by-fraction >td-print-politicians-by-fraction 2>&1
+td-download-referenced-documents >td-download-referenced-documents 2>&1
 
 echo All is good
 
