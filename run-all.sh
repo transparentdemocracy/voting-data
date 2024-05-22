0b1cf2872887517e0769a8234c75351bd5a0b67b
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-05-22 21:35:25 +0200

    Added basic langchain dependencies for summarization

--- run-all.sh
+++ run-all.sh
@@ -13,5 +13,7 @@ td-politicians-json >out/td-politicians-json 2>&1
 td-print-politicians-by-fraction >out/td-print-politicians-by-fraction 2>&1
 td-download-referenced-documents >out/td-download-referenced-documents 2>&1
 
+./convert-documents-to-text.sh
+
 echo All is good
 
