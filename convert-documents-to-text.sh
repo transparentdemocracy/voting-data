530ad992fc6390898ac097efcc1aa2aef2f1651d
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-05-25 14:07:58 +0200

    Simplify document conversion (no need for gnu parallel) + analysis of documents to summarize

--- convert-documents-to-text.sh
+++ convert-documents-to-text.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-function convert_parallel() {
-  find data/input/documents -name "*.pdf" | parallel pdftotext
-}
-
-function convert_noparallel() {
-  find data/input/documents -name "*.pdf" -exec pdftotext {} ';'
-}
-
 function convert_documents() {
     if ! which pdftotext > /dev/null; then
         echo "pdftotext is missing; try running brew install poppler"
     fi
-    if which parallel >/dev/null; then
-        convert_parallel
-    else
-        echo "parallel not found. Will fun without parallellism Install using `brew install parallel` to speed things up"
-        convert_noparallel
-    fi
+
+    find data/input/documents -name "*.pdf" | xargs -P 8 -I {} ./convert-single.sh {}
+
+    rsync -av --include="*/" --include="*.txt" --exclude="*" data/input/documents/ data/output/documents/txt/
+    find data/input/documents -name "*.txt" -delete
+
 }
+
+convert_documents
---
+++ convert-single.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pdftotext -x 0 -y 40 -W 595 -H 748 "$1"
