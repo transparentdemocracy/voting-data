d9c70a0592dfb9f54628586a5a1a9836e3a48f72
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-05-30 09:12:23 +0200

    Updated run-all to reflect usage changes

--- run-all.sh
+++ run-all.sh
@@ -11,12 +11,14 @@ td-plenaries-json >out/td-plenaries-json 2>&1
 td-votes-json >out/td-votes-json 2>&1
 td-politicians-json >out/td-politicians-json 2>&1
 td-print-politicians-by-fraction >out/td-print-politicians-by-fraction 2>&1
+
 td-download-referenced-documents >out/td-download-referenced-documents 2>&1
+./convert-documents-to-text.sh
+
 # Just as an example, doesn't summarize everything
-td-summarize 0 1000
+td-summarize nl 0 1000
 td-summaries-json
 
-./convert-documents-to-text.sh
 
 echo All is good
 
