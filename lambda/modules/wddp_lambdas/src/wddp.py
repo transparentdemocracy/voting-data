82bb55862b3b4bd2ec514de89fe0933eb87d5ada
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-11-03 15:43:07 +0100

    Sort by date descending

--- lambda/modules/wddp_lambdas/src/wddp.py
+++ lambda/modules/wddp_lambdas/src/wddp.py
@@ -2,48 +2,33 @@ import os
 
 import requests
 
+DEFAULT_TIMEOUT = 30
 PAGE_SIZE = 10
 
 
-def search_motions(event, context):
+def search_motions(event, _context):
     q = event.get("queryStringParameters", {}).get('q', "")
     page = int(event.get("queryStringParameters", {}).get('page', "0"))
 
-    return search("motions", q, page)
+    return search("motions", create_query(page, q, "votingDate"))
 
 
-def get_motion(event, context):
-    id = event.get("requestContext", {}).get("http", {})["path"][1:]
-    return get("motions", id)
+def get_motion(event, _context):
+    motion_id = event.get("requestContext", {}).get("http", {})["path"][1:]
+    return get("motions", motion_id)
 
 
-def search_plenaries(event, context):
+def search_plenaries(event, _context):
     q = event.get("queryStringParameters", {}).get('q', "")
     page = int(event.get("queryStringParameters", {}).get('page', "0"))
 
-    return search("plenaries", q, page)
+    return search("plenaries", create_query(page, q, "date"))
 
 
-def search(index, q, page):
-    query = {
-        "size": PAGE_SIZE,
-        "from": max(0, page) * PAGE_SIZE,
-        # "sort": [
-        #     {"date": {"order": "desc"}}
-        # ]
-
-    }
-
-    if q != "":
-        query["query"] = {
-            "query_string": {
-                "query": q
-            }
-        }
-
+def search(index, query):
     secret = os.environ['ES_AUTH']
-    url = "https://%s@transparent-democrac-6644447145.eu-west-1.bonsaisearch.net:443/%s/_search" % (secret, index)
-    response = requests.post(url, json=query)
+    url = f"https://{secret}@transparent-democrac-6644447145.eu-west-1.bonsaisearch.net:443/{index}/_search"
+    response = requests.post(url, json=query, timeout=DEFAULT_TIMEOUT)
     body = response.text
 
     return {
@@ -52,10 +37,29 @@ def search(index, q, page):
     }
 
 
-def get(index, id):
+def create_query(page, q, date_field):
+    query = {
+        "size": PAGE_SIZE,
+        "from": max(0, page) * PAGE_SIZE,
+        "sort": [
+            {date_field: {"order": "desc"}}
+        ]
+
+    }
+    if q != "":
+        query["query"] = {
+            "query_string": {
+                "query": q
+            }
+        }
+
+    return query
+
+
+def get(index, doc_id):
     secret = os.environ['ES_AUTH']
-    url = "https://%s@transparent-democrac-6644447145.eu-west-1.bonsaisearch.net:443/%s/_doc/%s" % (secret, index, id)
-    response = requests.get(url)
+    url = f"https://{secret}@transparent-democrac-6644447145.eu-west-1.bonsaisearch.net:443/{index}/_doc/{doc_id}"
+    response = requests.get(url, timeout=DEFAULT_TIMEOUT)
     body = response.text
 
     return {
