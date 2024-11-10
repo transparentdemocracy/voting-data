914635cd88915a5469322b0407fe7c4d7fd5a142
Karel Vervaeke <karel@vervaeke.info>
2024-11-10 17:21:04 +0100

    Fix download reports script + make sure we error out when LEGISLATURE is not set

--- download-reports.sh
+++ download-reports.sh
@@ -2,22 +2,25 @@
 
 set -e
 
-if [ -z "LEGISLATURE" ]; then
+if [ -z "$LEGISLATURE" ]; then
     echo "LEGISLATURE is not set. Set LEGISLATURE=56 for current legislature."
+    exit 1
 fi
 
 download_plenary_reports_html() {
   to=${1}
   HTML_DIR="data/input/plenary/html/leg-${LEGISLATURE}"
   mkdir -p "$HTML_DIR"
-  for i in $(seq -w "$to"); do
+  for i in $(seq -w -f "%03g" "$to"); do
     HTML_PATH="${HTML_DIR}/ip${i}x.html"
+    echo "$HTML_PATH"
     if [ ! -e "$HTML_PATH" ]; then
       echo "Downloading $HTML_PATH"
-      curl "https://www.dekamer.be/doc/PCRI/html/${LEGISLATURE}/ip${i}x.html" -o "$HTML_PATH"
+      URL="https://www.dekamer.be/doc/PCRI/html/${LEGISLATURE}/ip${i}x.html"
+      curl "$URL" -o "$HTML_PATH"
     fi
   done
 }
 
-max_plenary_nr=${1:-10}
+max_plenary_nr=${1:-11}
 download_plenary_reports_html ${max_plenary_nr}
--- transparentdemocracy/config.py
+++ transparentdemocracy/config.py
@@ -56,7 +56,7 @@ class Config:
 
 def _create_config():
     root_folder = os.path.dirname(os.path.dirname(__file__))
-    return Config(os.path.join(root_folder, "data"), legislature=os.environ.get("LEGISLATURE"))
+    return Config(os.path.join(root_folder, "data"), legislature=os.environ.get("LEGISLATURE", None))
 
 
 CONFIG = _create_config()
